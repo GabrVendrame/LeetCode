34f2c01525317e8a97e7f925a0d0206fa2c7ae06
Gabriel Vendrame <gabrielvendrame7@gmail.com>
2022-08-05 16:31:49 -0300

    Time: 2 ms (59.14%), Space: 6.2 MB (88.74%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -3,9 +3,13 @@ public:
     bool isSubsequence(string s, string t) {
         int find = 0;
         for(int i = 0; i < t.size(); i++){
-            if(s[find] == t[i]) find++;
+            if(s[find] == t[i]){
+                find++;
+            }
         }
-        if(find != s.size()) return false;
+        if(find != s.size()){
+            return false;   
+        }
         return true;
     }
 };
