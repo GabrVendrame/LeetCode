4d7e72aa448d92cdebccbeef7cfc4b955a6d71db
Gabriel Vendrame <gabrielvendrame7@gmail.com>
2022-08-25 21:34:59 -0300

    Time: 29 ms (98.78%), Space: 13.9 MB (14.75%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        res = []
+        pow_n = 0
+        while pow_n != 1:
+            pow_n = 0
+            while n > 0:
+                pow_n += pow((n % 10), 2)
+                n = n // 10
+            if pow_n in res:
+                return False
+            else:
+                n = pow_n
+                res.append(n)
+        return True
